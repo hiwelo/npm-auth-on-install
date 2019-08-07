4d578ea14b53be4a2ba9cce3e73680119e97db87
Damien Senger <damien@raccoon.studio>
2019-08-07 05:33:39 +0200

    Setup new command to build and run the command

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 
-// import chalk from 'chalk';
-import clear from 'clear';
-// import path from 'path';
-// import program from 'commander';
+import chalk from 'chalk';
+import path from 'path';
+import program from 'commander';
 
 /**
  * This is this script main file calling the core functions and features
@@ -13,9 +12,14 @@ import clear from 'clear';
  * Possible error codes:
  */
 
-(async (): Promise<void> => {
-  // first, we clear the console
-  clear();
+((): void => {
+  program
+    .version('0.0.1')
+    .description('Check and setup access to private NPM registry or package')
+    .option('-r', '--registry <registryUrl>', 'Private registry, to override package.json publishConfig')
+    .option('-p', '--package <packageName>', 'Private package, used to test access to the registry')
+    .parse(process.argv);
 
-  console.log('test');
+  console.log(chalk.red('test'));
+  console.log(program);
 })();

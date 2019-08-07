cbf93f89e06af603e2b65d011dc7aaf47657cc41
Damien Senger <damien@raccoon.studio>
2019-08-07 05:23:52 +0200

    Install TypeScript & core dependencies

--- src/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * This is this script main file calling the core functions and features
- *
- * --
- *
- * Possible error codes:
- */
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+import chalk from 'chalk';
+import clear from 'clear';
+import path from 'path';
+import program from 'commander';
+
+/**
+ * This is this script main file calling the core functions and features
+ *
+ * --
+ *
+ * Possible error codes:
+ */
+
